08d3f688c3ec0d76c55b0fc5bfca974c094cc8b9
Albert Zhang <albertzhang8148@gmail.com>
2025-09-20 16:43:20 -0400

    Sync Codespace with Latest Bolt Code

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -111,7 +111,6 @@ const Auth: React.FC = () => {
             </div>
           )}
 
-
           {/* Development Notice */}
           {!isAuth0Configured && (
             <div className="mb-4 p-3 bg-yellow-50 border border-yellow-200 rounded-lg">
